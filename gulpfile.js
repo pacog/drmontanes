f9c6855d158d10ee66778197ff17280f66e52d13
Paco Gomez Granados <pacogomezgranados@gmail.com>
2017-10-26 21:22:01 +0200

    Kind of pipe system for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 const gulp = require('gulp');
 const server = require('gulp-server-livereload');
+const sass = require('gulp-sass');
+const inlineCss = require('gulp-inline-css');
 
 gulp.task('webserver', function() {
-    gulp.src('app')
+    gulp.src('dist')
         .pipe(server({
             livereload: true,
             // directoryListing: true,
             open: true
     }));
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./app/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./app/sass/**/*.scss', ['sass', 'inject']);
+    gulp.watch('./app/*.html', ['inject']);
+});
+
+gulp.task('inject', function() {
+    return gulp.src('./app/*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('./dist/'));
+});
+
+// gulp.task('watch', ['sass:watch']);
+
+gulp.task('default', ['sass', 'inject', 'webserver', 'watch']);
