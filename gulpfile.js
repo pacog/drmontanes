b1f8976233dc67f5971d8b60c25f4a767fbcf5a1
Paco Gomez Granados <pacogomezgranados@gmail.com>
2017-10-29 11:39:41 +0100

    Automatically copying files and reloading styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const server = require('gulp-server-livereload');
 const sass = require('gulp-sass');
 const inlineCss = require('gulp-inline-css');
+const gulpsync = require('gulp-sync')(gulp);
 
 gulp.task('webserver', function() {
     gulp.src('dist')
@@ -13,14 +14,20 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./app/sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./app/sass/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./.tmp/css'));
+});
+
+gulp.task('copyAssets', function() {
+    return gulp.src('./app/assets/**/*.*')
+        .pipe(gulp.dest('./dist/assets'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./app/sass/**/*.scss', ['sass', 'inject']);
+    gulp.watch('./app/sass/**/*.scss', gulpsync.sync(['sass', 'inject']));
     gulp.watch('./app/*.html', ['inject']);
+    gulp.watch('./app/assets/**/*.*', ['copyAssets']);
 });
 
 gulp.task('inject', function() {
@@ -29,6 +36,5 @@ gulp.task('inject', function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-// gulp.task('watch', ['sass:watch']);
 
-gulp.task('default', ['sass', 'inject', 'webserver', 'watch']);
+gulp.task('default', gulpsync.sync(['sass', 'copyAssets', 'inject', 'webserver', 'watch']));
